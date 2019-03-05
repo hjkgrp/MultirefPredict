2e56b7dba5cdc9fc6948b95de435c408dd3c1840
Fang Liu <fangliu.ustc@gmail.com>
2019-03-05 10:58:02 -0500

    Removed unsued file compute.py. Changed the diagnostic_factory to be implemented with python dictionary

--- MultirefPredict/multirefpredict.py
+++ MultirefPredict/multirefpredict.py
@@ -7,7 +7,26 @@ Handles the primary functions
 from MultirefPredict.b1 import B1
 
 def diagnostic_factory(diagnostic_type, **kwargs):
-    if diagnostic_type == "B1":
-        return B1(**kwargs)
-    else:
+    """
+    factory class for calculating diagnostics
+
+    Parameters
+    ----------
+    diagnostic_type : str
+        currently support: {"B1"}
+        The type of diagnostic to calculate
+    keyword arguments:
+        molecule: qcelement molecule instance
+
+    Returns
+    -------
+    cls_instance:  instance of a class for calculating specific diagnostics
+    """
+    cls_dict = dict(B1=B1)
+    
+    if diagnostic_type not in cls_dict.keys():
         raise Exception("Diagnostic type not found")
+
+    cls = cls_dict[diagnostic_type]
+    cls_instance = cls(**kwargs)
+    return cls_instance
