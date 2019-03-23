fb44809efcbe413db0a38092c36e8bee6e2a94df
ChernuDuan <duanchenru@gmail.com>
2019-03-23 17:17:37 -0400

    Add C0 diagnostics based on CASSCF calculations.

--- MultirefPredict/multirefpredict.py
+++ MultirefPredict/multirefpredict.py
@@ -4,7 +4,9 @@ Automated workflow to predict multireference character of molecules in quantum c
 
 Handles the primary functions
 """
-from MultirefPredict.ebased_diagnostic import B1,A25PBE,TAE
+from MultirefPredict.ebased_diagnostic import B1, A25PBE, TAE
+from MultirefPredict.casbased_diagnostic import C0
+
 
 def diagnostic_factory(diagnostic_type, **kwargs):
     """
@@ -22,8 +24,8 @@ def diagnostic_factory(diagnostic_type, **kwargs):
     -------
     cls_instance:  instance of a class for calculating specific diagnostics
     """
-    cls_dict = dict(B1=B1,A25PBE=A25PBE,TAE=TAE)
-    
+    cls_dict = dict(B1=B1, A25PBE=A25PBE, TAE=TAE, C0=C0)
+
     if diagnostic_type not in cls_dict.keys():
         raise Exception("Diagnostic type not found")
 
---
+++ MultirefPredict/tests/test_casbased.py
@@ -0,0 +1,40 @@
+import pytest
+import qcelemental
+import qcengine
+from .compare import fuzzyEqual
+from MultirefPredict.casbased_diagnostic import obtainC0fromstr
+
+
+def test_casscf():
+    mol = qcelemental.models.Molecule(geometry=[0, 0, 0],
+                                      symbols=["O"],
+                                      molecular_charge=0,
+                                      molecular_multiplicity=3)
+    casscf_task = qcelemental.models.ResultInput(molecule=mol,
+                                                 driver="energy",
+                                                 model={"method": "casscf", "basis": "cc-pvdz"},
+                                                 keywords={"reference": "rohf"}
+                                                 )
+    results = qcengine.compute(casscf_task, "psi4")
+    energy = results.return_result
+    expected = -74.91174384584482
+    Thre = 1e-6
+    assert fuzzyEqual(energy, expected, Thre)
+
+
+def test_C0():
+    mol = qcelemental.models.Molecule(geometry=[[0, 0, 0], [0.7, 0, 0]],
+                                      symbols=["H", "H"],
+                                      molecular_charge=0,
+                                      molecular_multiplicity=1)
+    casscf_task = qcelemental.models.ResultInput(molecule=mol,
+                                                 driver="energy",
+                                                 model={"method": "casscf", "basis": "cc-pvdz"},
+                                                 keywords={"reference": "rohf"}
+                                                 )
+    results = qcengine.compute(casscf_task, "psi4")
+    outfile = results.stdout.split("\n")
+    diag = obtainC0fromstr(outfile)
+    expected = 0.995293
+    Thre = 1e-3
+    assert fuzzyEqual(diag, expected, Thre)
